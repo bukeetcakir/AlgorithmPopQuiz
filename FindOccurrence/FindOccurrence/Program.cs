20d91dc6ca127fb75ecb81a5845c11d27fa203aa
Buket <buket-cakir@hotmail.com>
2024-02-21 13:54:16 +0300

    Create ReverseText and OccurrenceFinder

---
+++ FindOccurrence/FindOccurrence/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace OccurenceFinder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+
+            Problem:
+            t and z are strings consist of lowercase English letters.
+
+            Find all substrings for t, and return the maximum value of [ len(substring) x [how many times the substring occurs in z] ]
+
+            Example:
+            t = acldm1labcdhsnd
+            z = shabcdacasklksjabcdfueuabcdfhsndsabcdmdabcdfa
+
+            Solution:
+            abcd is a substring of t, and it occurs 5 times in Z, abcd.Length x 5 = 20 is the solution
+
+            */
+
+
+            string FindOccurrence(string substring, string mainString)
+            {
+                int maxScore = 0;
+
+                for (int len = 1; len <= substring.Length; len++)
+                {
+                    for (int i = 0; i <= substring.Length - len; i++)
+                    {
+                        string pattern = substring.Substring(i, len);
+                        int count = CountOccurrences(mainString, pattern);
+                        int score = len * count;
+                        maxScore = Math.Max(maxScore, score);
+                    }
+                }
+
+                return maxScore.ToString();
+            }
+
+            int CountOccurrences(string source, string pattern)
+            {
+                int count = 0;
+                int i = 0;
+                while ((i = source.IndexOf(pattern, i, StringComparison.OrdinalIgnoreCase)) != -1)
+                {
+                    i += pattern.Length;
+                    count++;
+                }
+                return count;
+            }
+
+            var substring = "acldm1labcdhsnd";
+            var mainString = "shabcdacasklksjabcdfueuabcdfhsndsabcdmdabcdfa";
+
+            Console.WriteLine(FindOccurrence(substring, mainString));
+
+        }
+    }
+
+}
---
+++ ReverseText/ReverseText/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ReverseText
+{
+    /*
+You have a text that some of the words in reverse order.
+The text also contains some words in the correct order, and they are wrapped in parenthesis.
+Write a function fixes all of the words and,
+remove the parenthesis that is used for marking the correct words.
+
+Your function should return the same text defined in the constant correctAnswer, please keep in mind
+that shortest way possible will get you extra points.
+*/
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var inputText = "nhoJ (Griffith) nodnoL saw (an) (American) ,tsilevon ,tsilanruoj (and) laicos .tsivitca ((A) reenoip (of) laicremmoc noitcif (and) naciremA ,senizagam (he) saw eno (of) (the) tsrif (American) srohtua (to) emoceb (an) lanoitanretni ytirbelec (and) nrae a egral enutrof (from) ).gnitirw";
+            var correctAnswer = "John Griffith London was an American novelist, journalist, and social activist. (A pioneer of commercial fiction and American magazines, he was one of the first American authors to become an international celebrity and earn a large fortune from writing.)";
+
+
+            string ReverseText(string input)
+            {
+                string[] words = input.Split(' ');
+
+                var reversedWords = words.Select(word =>
+                {
+                    if (word.StartsWith("(") && word.EndsWith(")"))
+                    {
+                        return word.Substring(1, word.Length - 2);
+                    }
+                    else
+                    {
+                        return new string(word.Reverse().ToArray());
+                    }
+                });
+
+                var text = string.Join(" ", reversedWords);
+                return text.Trim();
+            }
+
+            Console.WriteLine(ReverseText(inputText) == correctAnswer ? "Correct !!!" : "Wrong answer...!");
+        }
+    }
+
+}
